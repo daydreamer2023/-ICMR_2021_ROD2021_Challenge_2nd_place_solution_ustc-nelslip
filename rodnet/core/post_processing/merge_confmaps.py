6ffa72ac1b24182a47a8de828f2bace7d8b32e93
haoxinl <35587846+haoxinl@users.noreply.github.com>
2021-03-29 10:36:36 +0800

    Add files via upload

---
+++ rodnet/core/post_processing/merge_confmaps.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class ConfmapStack:
+    def __init__(self, confmap_shape):
+        self.confmap = np.zeros(confmap_shape)
+        self.count = 0
+        self.next = None
+        self.ready = False
+
+    def append(self, confmap):
+        self.confmap = (self.confmap * self.count + confmap) / (self.count + 1)
+        # self.confmap =  confmap
+        self.count += 1
+
+    def setNext(self, _genconfmap):
+        self.next = _genconfmap
